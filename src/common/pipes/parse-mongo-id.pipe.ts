e2a26453415d20d5621ab92ec44c2d8543729c79
Rodrigo López Sarmiento <rodrigo.lopez@3ti.mx>
2023-02-19 19:33:31 -0600

    Seccion 7 terminada

---
+++ src/common/pipes/parse-mongo-id.pipe.ts
@@ -0,0 +1,16 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
+import { BadRequestException } from '@nestjs/common/exceptions';
+
+@Injectable()
+export class ParseMongoIdPipe implements PipeTransform {
+  transform(value: string, metadata: ArgumentMetadata) {
+    // console.log( { value, metadata } )
+    if( !isValidObjectId( value ) ){
+      throw new BadRequestException( `${ value } is not a valid MongoId` );
+    }
+
+
+    return value.toUpperCase();
+  }
+}
